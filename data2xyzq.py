f6cfe6eee8bc5387526ddaa4bbddd07c65b53af4
Wentao Zhang <zhangwentaoucas@gmail.com>
2024-07-26 18:04:40 +0200

    Simple visualization example

--- data2xyzq.py
+++ data2xyzq.py
@@ -1,14 +1,24 @@
 import pandas as pd
-#fileIN='EGMS_L3_E43N22_100km_U_2018_2022_1/EGMS_L3_E43N22_100km_U_2018_2022_1.csv'
-fileIN="EGMS_L3_E43N22_100km_E_2018_2022_1/EGMS_L3_E43N22_100km_E_2018_2022_1.csv"
-PA="10.53/42.91"
-PB="10.86/43.80"
+#fileIN="EGMS_L3_E43N22_100km_E_2018_2022_1/EGMS_L3_E43N22_100km_E_2018_2022_1.csv"
+fileIN_1="U/EGMS_L3_E42N21_100km_U_2018_2022_1/EGMS_L3_E42N21_100km_U_2018_2022_1.csv"
+fileIN_2="U/EGMS_L3_E43N21_100km_U_2018_2022_1/EGMS_L3_E43N21_100km_U_2018_2022_1.csv"
+fileIN_3="U/EGMS_L3_E43N22_100km_U_2018_2022_1/EGMS_L3_E43N22_100km_U_2018_2022_1.csv"
+fileIN_4="U/EGMS_L3_E44N22_100km_U_2018_2022_1/EGMS_L3_E44N22_100km_U_2018_2022_1.csv"
+
+PA="9.696/42.617"
+PB="21.0/47.343"
+region_map = [6, 22,41, 48]
+
 fileOUT="OUT.csv" # more infor of format: https://docs.generic-mapping-tools.org/6.5/project.html
 
-Plot = 0  # 0:No plot the figure
+Plot = 1  # 0:No plot the figure
 
 # read csv file, more infor of format: https://land.copernicus.eu/en/products/european-ground-motion-service
-df = pd.read_csv(fileIN)
+df1 = pd.read_csv(fileIN_1)
+df2 = pd.read_csv(fileIN_2)
+df3 = pd.read_csv(fileIN_3)
+df4 = pd.read_csv(fileIN_3)
+df = pd.concat([df1, df2, df3, df4], ignore_index=True)
 print(df.head())
 
 # ----------------------------------------------------------------------------
@@ -59,8 +69,8 @@ if Plot == 0:
 else:
     print("A is not 1, continuing the code.")
 
-region_map = [8, 16,41, 44]
-region_map = [df['lon'].min(), df['lon'].max(),df['lat'].min(), df['lat'].max()]
+# region_map = [df['lon'].min(), df['lon'].max(),df['lat'].min(), df['lat'].max()]
+
 
 fig = pygmt.Figure()
 fig.basemap(region=region_map, projection="M12c", frame="af",)
@@ -76,7 +86,7 @@ fig.colorbar(
 )
 
 
-fig.plot(x=df['lon'], y=df['lat'], style="s.1c", fill="seagreen")
+fig.plot(x=df['lon'], y=df['lat'], style="s.01c", fill="black")
 
 # Choose a survey line
 track_df = pygmt.project(
@@ -88,4 +98,5 @@ track_df = pygmt.project(
 Profile = pygmt.project(center=PA, endpoint=PB, generate=10)
 fig.plot(x=Profile.r, y=Profile.s, pen="1p,red,dashed")
 
-fig.show()
+fig.savefig('Map.pdf')
+# fig.show()
--- plot_xyzq.py
+++ plot_xyzq.py
@@ -4,19 +4,38 @@ import pygmt
 # PA="10.53/42.91"
 # PB="10.86/43.80"
 fileOUT="OUT.csv"
-Wigth=10 #  The max distance with the selected profile
-
-df = pd.read_csv(fileOUT)
-df.plot(kind='scatter', x='p', y='mean_velocity', figsize=(10, 6))
-plt.xlabel('Distance/km')
-plt.ylabel('mean_velocity')
-plt.title('All')
-plt.show()
+Wigth=25 #  The max distance (km) with the selected profile
 
 
-filtered_df = df[df['q'].abs() < Wigth]
-filtered_df.plot(kind='scatter', x='p', y='mean_velocity', figsize=(10, 6))
-plt.xlabel('Distance/km')
-plt.ylabel('mean_velocity')
-plt.title('10')
-plt.show()
+
+df_all = pd.read_csv(fileOUT)
+df = df_all[df_all['q'].abs() < Wigth]
+# 创建一个2行1列的子图结构
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(12, 8), sharex=True)
+
+# 第一个子图
+scatter = axes[0].scatter(df_all['p'], df_all['q'], c=df_all['mean_velocity'], cmap='rainbow', s=1.0, vmin=-20, vmax=20) # rainbow, viridis
+axes[0].axhline(y=Wigth, color='red', linestyle='--', linewidth=2, label='y = 10')
+axes[0].axhline(y=-Wigth, color='red', linestyle='--', linewidth=2, label='y = 10')
+colorbar = fig.colorbar(scatter, ax=axes[0])
+colorbar = fig.colorbar(scatter, ax=axes[1])
+colorbar.set_label('mean_velocity, mm/y')
+axes[0].set_title('Scatter Plot of All')
+axes[0].set_xlabel('Distance/km')
+axes[0].set_ylabel('Distance from Projection to Start/km')
+axes[0].grid(True)
+
+
+
+# 第二个子图
+axes[1].scatter(df['p'], df['mean_velocity'], c='blue', s=1.0)
+axes[1].set_title(f'The mean_velocity along the profile (< %.1f km)' % Wigth)
+
+axes[1].set_xlabel('Distance/km')
+axes[1].set_ylabel('mean_velocity')
+axes[1].grid(True)
+
+plt.tight_layout()
+
+plt.savefig('Profile.png')
+# plt.show()
